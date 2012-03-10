6ee7ff9e5d642e46e97e11379670c9db4edafdb2
kristina <kristina@kristina-laptop>
2012-03-10 00:43:06 -0800

    Implemented a trivial searcher function in Searcher.

--- BitNom/src/bitNom/Searcher.java
+++ BitNom/src/bitNom/Searcher.java
@@ -1,5 +1,10 @@
 package bitNom;
-
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Vector;
+import java.lang.String;
 //TODO:
 // Decide when we "stop" the search. Perhaps give the query a total lifespan.
 //		or maybe record the path we've already taken.
@@ -14,5 +19,34 @@ package bitNom;
 // Also takes care of finding a file with a specific hash.
 
 public class Searcher implements Runnable{
+	//static File directory;
+	
+	public Searcher()
+	{
+		super();
+		//directory = new File(path);
+	}
+	
+	public static Collection<File> matchingFiles(File root, String pattern)
+	{
+		File[] entries = root.listFiles();
+		Vector<File> files = new Vector<File>();
+		if(entries != null)
+		{
+			for(File entry : entries)
+			{
+					if(entry.isDirectory())
+					{
+						files.addAll(matchingFiles(entry, pattern));
+					}
+					else if(entry.getName().contains(pattern))
+					{
+						files.add(entry);
+					}				
+			}
+		}
+		return files;
+	}
+	
 	public void run(){}
 }
