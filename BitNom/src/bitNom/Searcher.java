034955033511a3ef901259017405fb8f63f76470
kristina <kristina@kristina-laptop>
2012-03-12 16:11:31 -0700

    Updated Searcher. Also included documentation.

--- BitNom/src/bitNom/Searcher.java
+++ BitNom/src/bitNom/Searcher.java
@@ -9,80 +9,89 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Vector;
 import java.lang.String;
-//TODO:
-// Decide when we "stop" the search. Perhaps give the query a total lifespan.
-//		or maybe record the path we've already taken.
-
-// Given a search query, search your own node to see if you have any relevant
-// files. If so, return a list of paths to your relevant files.
-
-// Depending on if the content is found on this machine, or some query lifetime,
-// dunno yet, forward the query to other peers and forward their results back to
-// the original requestor.
-
-// Also takes care of finding a file with a specific hash.
+/*
+ * Currently, searchDirectory() is called in the constructor of the Searcher class. searchDirectory()
+ * is responsible for creating .search, a file which contains a list of all filenames within a given 
+ * directory (namespace). In keeping like with like, I'm comfortable with keeping searchDirectory() here.
+ * However, in order for searches to be performed by other peers, .search should be created at the earliest
+ * possible moment - the Searcher class must be initialized as early as possible.
+ * 
+ * An actual search is performed with the searchThroughPeers() function. It is a public function that can
+ * be called whenever. It requires a list of peers. It functions by downloading each peer's .search file,
+ * truncating that .search file based on the query and adding it to a .results file. At the end of this function,
+ * all results are displayed on the console for the user to choose from. The selection is then downloaded.
+ */
 
 public class Searcher implements Runnable{
 	File root;
-	Collection<File> queryResults;
 	
-	public Searcher(String home, String query)
+	public Searcher(String home)
 	{
 		super();
 		root = new File(home);
-		queryResults = matchingFiles(root, query);
-		
-		// Write query results to .search file on the local repo
-		writeToSearch();
+		searchDirectory(root);
 	}
 	
-	private Collection<File> matchingFiles(File root, String pattern)
+	/* searchDirectory() searches recursively through a given directory (root).
+	 * It records all filenames a local file, called .search.
+	 */
+	private void searchDirectory(File root)
 	{
 		File[] entries = root.listFiles();
-		Vector<File> files = new Vector<File>();
 		if(entries != null)
 		{
 			for(File entry : entries)
 			{
-					String filename = entry.getPath();
-					boolean match = filename.substring(filename.lastIndexOf("/") + 1).compareTo(pattern) == 0;
-					if(entry.isDirectory())
-					{
-						files.addAll(matchingFiles(entry, pattern));
-					}
-					else if(match)
-					{
-						files.add(entry);
-					}				
+				//String filename = entry.getPath();
+				//boolean match = filename.substring(filename.lastIndexOf("/") + 1).compareTo(pattern) == 0;
+				if(entry.isDirectory())
+				{
+					searchDirectory(entry);
+				}
+				else if(entry.isFile())
+				{
+					writeToSearch(entry);
+				}				
 			}
 		}
-		return files;
 	}
 	
-	private void writeToSearch()
+	/* writeToSearch() is a helper function for searchDirectory().
+	 * It handles the actual creation and writing to .search.
+	 */
+	private void writeToSearch(File file)
 	{
-		try {
-			FileWriter fstream = new FileWriter(root.getPath() + "/.search");
-			BufferedWriter out = new BufferedWriter(fstream);
-			for(File file : queryResults)
-			{
-				String relative =  root.toURI().relativize(file.toURI()).getPath();
-				String pathname = root + "/" + relative;
-				try {
-					System.out.println("Writing to: " + root.getPath() + "/.search");
-					out.write(pathname);
-					out.newLine();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		File searchFile = new File(root.getAbsolutePath() + "/.search");
+		// Create the search file, if one does not already exist.
+		if(!searchFile.exists())
+		{
+			try {
+				searchFile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		}
+		
+		// Append filename to .search
+		try {
+			FileWriter fstream = new FileWriter(searchFile.getAbsolutePath(), true); // true -> append
+			BufferedWriter out = new BufferedWriter(fstream);
+			
+			String relative =  root.toURI().relativize(file.toURI()).getPath();
+			String pathname = root + "/" + relative;
+			try {
+				System.out.println("Writing to: " + searchFile.getAbsolutePath());
+				out.write(pathname);
+				out.newLine();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			try {
 				out.close();
-				System.out.println("Done.");
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -93,9 +102,35 @@ public class Searcher implements Runnable{
 		}
 	}
 	
+	/* searchThroughPeers() looks through the .search files of its peers,
+	 * for a given query. It outputs a list of matches for the user to choose
+	 * from.
+	 */
 	public void searchThroughPeers(ArrayList<String> peers)
 	{
-		// Create a .results file
+		File resultsFile = new File(root.getAbsolutePath() + "/.results");
+		File searchHelper = new File(root.getAbsolutePath() + "/.searchhelper");
+		// Create the .results file, if one does not already exist.
+		if(!resultsFile.exists())
+		{
+			try {
+				resultsFile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		// Create the .searchhelper file
+		if(!searchHelper.exists())
+		{
+			try {
+				searchHelper.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		for(String peer : peers)
 		{
 			// Download .search from peer. Name the file .searchhelper
@@ -105,7 +140,7 @@ public class Searcher implements Runnable{
 		// Output to screen the file list.
 		FileInputStream fstream;
 		try {
-			fstream = new FileInputStream(root.getPath() + "/.results");
+			fstream = new FileInputStream(root.getAbsolutePath() + "/.results");
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			String strLine;
@@ -132,6 +167,15 @@ public class Searcher implements Runnable{
 		
 		// Optionally, we could check to see if there is only one result and
 		// download that automatically.
+	}
+	
+	/* truncateToQuery() is a helper function for searchThroughPeers().
+	 * searchThroughPeers() passes a query to truncateToQuery(). truncateToQuery()
+	 * truncates .searchhelper based on this query.
+	 */
+	private void truncateToQuery(String query)
+	{
+		
 	}
 	public void run(){}
 }
