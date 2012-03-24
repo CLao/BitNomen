d720fd9c6ae117ef8f0c33d2fc14c0e64bed961d
Christian Lao <clao@ucla.edu>
2012-03-23 19:26:48 -0700

    Implemented ui skeleton

--- BitNom/src/bitNom/BitNom.java
+++ BitNom/src/bitNom/BitNom.java
@@ -1,5 +1,9 @@
 package bitNom;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BitNom {
 	
 	public static Server server;
@@ -25,22 +29,11 @@ public class BitNom {
 		peerLgr = new PeerLogger();
 		(new Thread (peerLgr)).start();
 		
-		/*
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
 		downloadMgr = new DownloadManager(peerLgr);
 		(new Thread (downloadMgr)).start();
 		
 		searcher = new Searcher();	
 		startBitNom();
-
-		
-	
 
 	}
 	
@@ -57,7 +50,45 @@ public class BitNom {
 	}
 	
 	public static void startBitNom(){
-		test();
+		//test();
+		Boolean quit = false;
+		String input;
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("BitNomen v0.0.1\n COMMANDS:\n\tDOWNLOAD <ccn filepath>\n\tSEARCH <query>\n\tSTATUS\n\tQUIT");
+		
+		while(!quit){
+			try {
+				input = in.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Error reading standard input.");
+				input = "";
+			}
+			
+			if(input.matches("DOWNLOAD[\\s]+.*")) {
+				System.out.println("Downloading " + input.split("DOWNLOAD[\\s]+.*")[0]);
+			}
+			
+			else if(input.matches("SEARCH[\\s]+.*")) {
+				System.out.println("Searching for " + input.split("SEARCH[\\s]+.*")[0]);
+			}
+			
+			else if(input.matches("STATUS")) {
+				downloadMgr.printStatus(false);
+			}
+			
+			else if(input.matches("QUIT")) {
+				System.out.println("Goodbye!");
+				quit = true;
+				System.exit(0);
+			}
+			
+			else {
+				System.out.println("Invalid input: " + input);
+				input = "";
+			}
+		}
 	}
 
 }
